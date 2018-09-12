e8dee9d7a4724ad8249e281d498edfb284db4f8d
Hue Nam <huenam.ly@gmail.com>
2018-09-12 16:06:28 +0200

    add tp1

---
+++ TP/ACL/src/matiere/Matiere.java
@@ -0,0 +1,14 @@
+package matiere;
+
+public class Matiere {
+	
+	protected String nom;
+	
+	public Matiere( String nom){
+		this.nom = nom;
+	}
+	
+	public String toString(){
+		return this.nom;
+	}
+}
---
+++ TP/ACL/src/personne/Etudiant.java
@@ -0,0 +1,35 @@
+package personne;
+
+
+import java.util.Calendar;
+
+
+public class Etudiant extends Personne{
+	protected int age;
+
+	protected static int NOETU;
+	protected int noEt;
+	
+	public Etudiant(String nom, String prenom, int annee){
+		super(nom, prenom);
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		this.age = year - annee;
+		noEt = NOETU++;
+	}
+	
+	public String toString(){
+
+		StringBuilder s = new StringBuilder("");
+		s.append("Etudiant " + this.prenom + " " + this.nom + "(noEt=" + noEt+ ") " + this.age + " ans");
+		return s.toString();
+	}
+	
+
+	public static void main(String[] args) {
+			Personne e1 = new Etudiant("Ly", "nam", 1994);
+			System.out.println(e1.toString());
+	}
+	
+}
+
+
